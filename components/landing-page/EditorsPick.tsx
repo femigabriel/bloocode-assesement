8e26ae3ceb471bdc96f923ba0208d73d9d9d186c
Femi Gabriel <timfemi@gmail.com>
2025-04-17 12:22:46 +0100

    first commit

--- components/landing-page/EditorsPick.tsx
+++ components/landing-page/EditorsPick.tsx
@@ -1,7 +1,14 @@
-import React from 'react'
+import React from 'react';
 
 export const EditorsPick = () => {
   return (
-    <div>EditorsPick</div>
-  )
-}
+    <section className="px-4 py-8">
+      <div className="mb-4">
+        <h3 className="text-2xl font-semibold">EDITOR’S PICKS</h3>
+        <p className="text-[#5A5A5A] text-sm border-l-[3px] border-[#CC0001] pl-3">
+          Featured Episodes
+        </p>
+      </div>
+    </section>
+  );
+};
