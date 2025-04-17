ec69b7143098a8dbf88b49bc7e196e00d13191a8
Femi Gabriel <timfemi@gmail.com>
2025-04-17 12:32:58 +0100

    first commit

--- components/landing-page/EditorsPick.tsx
+++ components/landing-page/EditorsPick.tsx
@@ -1,13 +1,26 @@
 import React from 'react';
+import Image from "next/image";
 
 export const EditorsPick = () => {
   return (
     <section className="px-4 py-8">
       <div className="mb-4">
         <h3 className="text-2xl font-semibold">EDITOR’S PICKS</h3>
-        <p className="text-[#5A5A5A] text-sm border-l-[3px] border-[#CC0001] pl-3">
+        <p className="text-[#5A5A5A] text-sm border-l-[3px] border-[#CC0001] pl-2">
           Featured Episodes
         </p>
+      </div>
+      <div className="py-4 flex">
+      <div className="">
+      <Image
+        src="/assets/images/Group 1496 (1).svg"
+        alt="hero content"
+        width={800}
+        height={300}
+        className="mx-auto w-full max-w-[800px] object-cover"
+        draggable="false"
+      />
+    </div>
       </div>
     </section>
   );
--- components/landing-page/HeroBanner.tsx
+++ components/landing-page/HeroBanner.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import React from "react";
 import Image from "next/image";
 
@@ -17,53 +15,55 @@ const HeroBanner = () => (
         />
       </div>
 
-      <div className="flex-1 bg-[#1B1B1B] h-full flex items-center justify-center">
-        <nav className="flex justify-between text-white w-full max-w-4xl px-6 text-sm">
-          <li className="flex items-center gap-2 list-none">
-            <Image
-              src="/assets/icons/book-alt 1.svg"
-              alt="icon"
-              width={18}
-              height={18}
-              draggable="false"
-            />
-            <span>Latest News</span>
-          </li>
-          <li className="flex items-center gap-2 list-none">
-            <Image
-              src="/assets/icons/microphone-alt 1.svg"
-              alt="icon"
-              width={18}
-              height={18}
-              draggable="false"
-            />
-            <span>New Episodes</span>
-          </li>
-          <li className="flex items-center gap-2 list-none">
-            <Image
-              src="/assets/icons/boxes 1.svg"
-              alt="icon"
-              width={18}
-              height={18}
-              draggable="false"
-            />
-            <span>Our Services</span>
-          </li>
-          <li className="flex items-center gap-2 list-none">
-            <Image
-              src="/assets/icons/podcast (1) 1.svg"
-              alt="icon"
-              width={18}
-              height={18}
-              draggable="false"
-            />
-            <span>All Podcasts</span>
-          </li>
-        </nav>
+      <div className="flex-1 bg-[#1B1B1B] h-full overflow-hidden">
+        <div className="w-full h-full flex items-center">
+          <nav className="flex gap-12 animate-marquee whitespace-nowrap text-white text-sm px-6">
+            <li className="flex items-center gap-2 list-none">
+              <Image
+                src="/assets/icons/book-alt 1.svg"
+                alt="icon"
+                width={18}
+                height={18}
+                draggable="false"
+              />
+              <span>Latest News</span>
+            </li>
+            <li className="flex items-center gap-2 list-none">
+              <Image
+                src="/assets/icons/microphone-alt 1.svg"
+                alt="icon"
+                width={18}
+                height={18}
+                draggable="false"
+              />
+              <span>New Episodes</span>
+            </li>
+            <li className="flex items-center gap-2 list-none">
+              <Image
+                src="/assets/icons/boxes 1.svg"
+                alt="icon"
+                width={18}
+                height={18}
+                draggable="false"
+              />
+              <span>Our Services</span>
+            </li>
+            <li className="flex items-center gap-2 list-none">
+              <Image
+                src="/assets/icons/podcast (1) 1.svg"
+                alt="icon"
+                width={18}
+                height={18}
+                draggable="false"
+              />
+              <span>All Podcasts</span>
+            </li>
+          </nav>
+        </div>
       </div>
     </div>
 
-    <div className="py-4 px-4 bg-white">
+    <div className="py-4 px-4">
       <Image
         src="/assets/images/Group 1086.svg"
         alt="hero content"
